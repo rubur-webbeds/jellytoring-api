9dd89024c99dbb20ae1559171909a3e82c151849
Ruben Bustos <ruben_b_r_@hotmail.com>
2021-08-09 19:47:25 +0200

    Convert CreateUser to class

--- jellytoring-api/Models/Users/CreateUser.cs
+++ jellytoring-api/Models/Users/CreateUser.cs
@@ -1,12 +1,14 @@
 ﻿namespace jellytoring_api.Models.Users
 {
-    public record CreateUser(
-        string FullName,
-        string Email,
-        string Password,
-        uint InterestId,
-        string Institution,
-        string CountryCode,
-        bool GrantContactInfoPermission,
-        bool GrantUibPermission);
+    public class CreateUser
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public uint InterestId { get; set; }
+        public string Institution { get; set; }
+        public string CountryCode { get; set; }
+        public bool GrantContactInfoPermission { get; set; }
+        public bool GrantUibPermission { get; set; }
+    }
 }
