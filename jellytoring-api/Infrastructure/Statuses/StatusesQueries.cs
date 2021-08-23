2923c6a23d6a71d4807cc9cdaca38610ba33f4d7
Ruben Bustos <ruben_b_r_@hotmail.com>
2021-08-24 00:43:09 +0200

    Add statuses repository

---
+++ jellytoring-api/Infrastructure/Statuses/StatusesQueries.cs
@@ -0,0 +1,7 @@
+﻿namespace jellytoring_api.Infrastructure.Statuses
+{
+    public static class StatusesQueries
+    {
+        public const string GetAll = "select id Id, code Code, name Name from statuses";
+    }
+}
---
+++ jellytoring-api/Infrastructure/Statuses/StatusesRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using jellytoring_api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace jellytoring_api.Infrastructure.Statuses
+{
+    public class StatusesRepository : IStatusesRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+
+        public StatusesRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task<IEnumerable<Status>> GetAllAsync()
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.OpenAsync();
+
+            return await connection.QueryAsync<Status>(StatusesQueries.GetAll);
+        }
+    }
+}
